38780716177f74e4140d0baf2e5ad33b7c80e7e5
Sam Dryden <flint_sd@hotmail.com>
2026-01-13 07:41:49 +0000

    chore: add pm2 env_production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,14 @@ module.exports = {
     {
       name: "xero-bridge",
       script: "xero-bridge.js",
+
       env: {
+        NODE_ENV: "development",
+        PORT: process.env.PORT || 4002
+      },
+
+      env_production: {
+        NODE_ENV: "production",
         PORT: process.env.PORT || 4002
       }
     }
